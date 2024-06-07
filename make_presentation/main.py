d88e7c91413db146f44904d056b1e2fb317949f9
Ekaterina Itchenko <ekaterina.savickaya.1989@gmail.com>
2024-06-07 10:32:20 +0300

    Add print in main

--- make_presentation/main.py
+++ make_presentation/main.py
@@ -2,23 +2,28 @@ import asyncio
 import os
 
 from config import path_to_file
-from presentation import Presentation
 from presentation_pptx import PresentationPPTX
 
-theme = "Поэзия Росии золотой век"
+#from presentation import Presentation       # noqa E800
+theme = input("Введите тему презентации: ")
 
-pr = PresentationPPTX(theme=theme)
+pr = PresentationPPTX(theme=theme)   # pr1 = Presentation(theme=theme)
 save_path_for_images = os.path.join(path_to_file, "images")
-
-pr1 = Presentation(theme=theme)
 
 
 async def main():
     if __name__ == "__main__":
+        print("START")                                 # noqa T201
+        import time
+        st = time.time()
         task1 = asyncio.create_task(
-            pr1.make_presentation(save_path_for_images=save_path_for_images)
+            pr.make_presentation(
+                save_path_for_images=save_path_for_images
+            )
         )
-        await task1
+        t = await task1
+        print(t)                                       # noqa T201     
+        print(f"TIME : {(time.time() - st)/60}")       # noqa T201
 
 
 asyncio.run(main())
