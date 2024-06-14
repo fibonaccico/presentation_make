92dff6874f48ff5df20e055f8834638d6f384486
Ekaterina Itchenko <ekaterina.savickaya.1989@gmail.com>
2024-06-14 16:28:40 +0300

    Fix main.py

--- make_presentation/main.py
+++ make_presentation/main.py
@@ -1,18 +1,17 @@
 import asyncio
 import os
 
-from presentation import Presentation  # noqa E800
-
 from make_presentation.config import path_to_file
+from make_presentation.presentation import Presentation  # noqa E800
 from make_presentation.presentation_pptx import PresentationPPTX
 
-choice = input("Введите тему презентации:\n1. По теме\n2. По тексту")
+choice = input("\n1. По теме\n2. По тексту\nВыберите тип генерации презентации: ")
 if choice == "1":
     theme = input("Введите тему презентации: ")
     text = ''
     pr = PresentationPPTX(text_generation_model="TWOSTEP", theme=theme)
 elif choice == "2":
-    text = input("Введите тему презентации: ")
+    text = input("Введите текст: ")
     pr = PresentationPPTX(text_generation_model="FROMTEXT")
 
 
