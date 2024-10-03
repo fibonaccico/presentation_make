6065bd8b55ed5b2b54c988d660eb76b6811d15dd
Ekaterina Itchenko <ekaterina.savickaya.1989@gmail.com>
2024-10-03 21:14:41 +0300

    Add flow and focus pptx files in MANIFEST

--- make_presentation/generator_models/pptx/slide.py
+++ make_presentation/generator_models/pptx/slide.py
@@ -99,7 +99,6 @@ class Slide:
                         if self.text_color else None
                     )
                     shape_text = shape.text
-                    logger.info(f"Shape text {shape_text}. Last item {shape_text[-1]}")
                     text_number = int(shape_text[-1])
                     self.__add_text_to_placeholder(
                         text_placeholder=shape,
