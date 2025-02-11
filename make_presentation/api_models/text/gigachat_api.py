c9ee2b953e07efa9b06f005d2b9936d02654a732
misaev <misaev@ptsecurity.com>
2025-02-11 22:01:09 +0700

    remove semaphore, gigachat temp 0.8

--- make_presentation/api_models/text/gigachat_api.py
+++ make_presentation/api_models/text/gigachat_api.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import asyncio
+# import asyncio
 import logging
 import os
 
@@ -10,8 +10,7 @@ from langchain.schema import HumanMessage
 
 from make_presentation.api_models.errors import BlacklistError
 from make_presentation.api_models.interfaces import TextAPIProtocol
-from make_presentation.config import (DEFAULT_REQUEST_NUMBER,
-                                      DEFAULT_TEMPERATURE)
+from make_presentation.config import DEFAULT_TEMPERATURE
 
 load_dotenv()
 logger = logging.getLogger(__name__)
@@ -24,7 +23,7 @@ class GigaChatRequest(TextAPIProtocol):
             scope="GIGACHAT_API_B2B",
             verify_ssl_certs=False
         )
-        self.semaphore = asyncio.Semaphore(value=DEFAULT_REQUEST_NUMBER)
+        # self.semaphore = asyncio.Semaphore(value=DEFAULT_REQUEST_NUMBER)
         self.api.temperature = DEFAULT_TEMPERATURE
         # self.history = []  # noqa E800
 
@@ -32,13 +31,13 @@ class GigaChatRequest(TextAPIProtocol):
         self,
         text: str
     ) -> str | list[str | dict]:
-        async with self.semaphore:
+        # async with self.semaphore:
         # self.history.append(HumanMessage(content=text))   # noqa E800
         # response = await self.api.ainvoke(self.history)   # noqa E800
         # self.history.append(response)                     # noqa E800
-            request_cost = await self.api.atokens_count(input_=[text])
-            response = await self.api.ainvoke([HumanMessage(content=text)])
-            response_cost = await self.api.atokens_count(input_=[response.content])
+        request_cost = await self.api.atokens_count(input_=[text])
+        response = await self.api.ainvoke([HumanMessage(content=text)])
+        response_cost = await self.api.atokens_count(input_=[response.content])
 
         logger.info(
             f'Request costs [{request_cost[0]}] tokens.'
